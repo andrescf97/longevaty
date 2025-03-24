d457c3ef22a60bf506df12c2451c1a75a5c7ec06
tsch.maulikk <maulikk.chevli@tum.de>
2025-03-24 21:51:31 +0100

    Remove reduced monai dict

--- survival.py
+++ survival.py
@@ -56,9 +56,6 @@ def main(cfg: Config):
     with open(cfg.data.monai_dict_dev) as fp:
         monai_dict_dev = json.load(fp)
 
-    monai_dict_train = monai_dict_train[:100]
-    monai_dict_dev = monai_dict_dev[:100]
-
     train_censoring_distribution = get_censoring_dist(monai_dict_train)
     
     train_transforms = make_transformations(tf_dict=cfg.transform.train_tf)
